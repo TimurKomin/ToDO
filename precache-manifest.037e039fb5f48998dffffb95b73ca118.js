f770d5f392ede13d8c2ed65e4cf1560894ac4144
OEM Configuration (temporary user) <timurkomin9@gmail.com>
2022-05-24 11:29:41 +0300

    Updates

---
+++ precache-manifest.037e039fb5f48998dffffb95b73ca118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d1446962769e0c65516",
+    "url": "/ToDO/static/css/main.e89a1f7d.chunk.css"
+  },
+  {
+    "revision": "7d1446962769e0c65516",
+    "url": "/ToDO/static/js/main.7d144696.chunk.js"
+  },
+  {
+    "revision": "0a3f203cf62175b309ed",
+    "url": "/ToDO/static/js/1.0a3f203c.chunk.js"
+  },
+  {
+    "revision": "aea2041aa838a72b19fb",
+    "url": "/ToDO/static/js/runtime~main.aea2041a.js"
+  },
+  {
+    "revision": "5b9acd9a4234a21b2be7b0cac87e70b0",
+    "url": "/ToDO/index.html"
+  }
+];
